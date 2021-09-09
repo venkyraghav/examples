4d9097a7c5d61411ab3edb6c9db7368c152573d5
Venky Narayanan <venkyraghav@gmail.com>
2021-09-09 12:05:00 -0400

    Created GenericRecord example

--- fincard/src/main/java/io/confluent/examples/fincard/streams/FinCardSpendStream.java
+++ fincard/src/main/java/io/confluent/examples/fincard/streams/FinCardSpendStream.java
@@ -4,6 +4,10 @@ import io.confluent.examples.fincard.TransactionRequest;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.reflect.ReflectData;
 import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
@@ -14,14 +18,19 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import scala.sys.Prop;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 
 @Service
@@ -105,12 +114,12 @@ public class FinCardSpendStream {
         startStream(builder, streamsConfiguration);
     }
 
-    @Bean
-    public void spendByZipcode() {
+    // @Bean
+    public void spendByZipcodeOld() {
         String fromTopic = "test2";
-        String toTopic = "spendbyzipcode";
+        String toTopic = "spendbyzipcodeold";
 
-        if (log.isInfoEnabled()) {log.info("spendByZipcode stream started from %s to %s", fromTopic, toTopic);}
+        if (log.isInfoEnabled()) {log.info("spendByZipcodeOld stream started from %s to %s", fromTopic, toTopic);}
 
         Properties streamsConfiguration = getStreamProperties("zipcode");
         final Serde<String> stringSerde = Serdes.String();
@@ -126,4 +135,76 @@ public class FinCardSpendStream {
 
         startStream(builder, streamsConfiguration);
     }
+
+    @Bean
+    public void spendByZipcode() {
+        String fromTopic = "test2";
+        String toTopic = "spendbyzipcode";
+
+        if (log.isInfoEnabled()) {log.info("spendByZipcode stream started from %s to %s", fromTopic, toTopic);}
+
+        Properties streamsConfiguration = getStreamProperties("zipcode");
+        final Map<String, String> serdeConfig = Collections.singletonMap(
+                AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        // streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
+        final Serde<GenericRecord> genericAvroSerde = new GenericAvroSerde();
+        genericAvroSerde.configure(serdeConfig, true);
+        final Serde<SpecificRecord> transactionRequestSerde = new SpecificAvroSerde<SpecificRecord>();
+        transactionRequestSerde.configure(serdeConfig, false);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, SpecificRecord> stream = builder.stream(fromTopic);
+        final KStream<GenericRecord, SpecificRecord> zipcodeRekey = stream.map(
+                (transactionId, transactionRecord) -> {
+                    LocationGroup locationGroup = new LocationGroup();
+                    locationGroup.setCardnumber(transactionRecord.get(cardinal_cardnumber).toString());
+                    locationGroup.setZipcode(transactionRecord.get(cardinal_zipcode).toString());
+
+                    GenericRecord record = mapObjectToRecord(locationGroup);
+                    return new KeyValue<GenericRecord, SpecificRecord>(record, transactionRecord);
+                }
+        );
+        zipcodeRekey.to(toTopic, Produced.with(genericAvroSerde, transactionRequestSerde));
+
+        startStream(builder, streamsConfiguration);
+    }
+
+    private GenericData.Record mapObjectToRecord(Object object) {
+        Assert.notNull(object, "object must not be null");
+        final Schema schema = ReflectData.get().getSchema(object.getClass());
+        final GenericData.Record record = new GenericData.Record(schema);
+        schema.getFields().forEach(r -> record.put(r.name(), PropertyAccessorFactory.forDirectFieldAccess(object).getPropertyValue(r.name())));
+        return record;
+    }
+
+    private <T> T mapRecordToObject(GenericData.Record record, T object) {
+        Assert.notNull(record, "record must not be null");
+        Assert.notNull(object, "object must not be null");
+        final Schema schema = ReflectData.get().getSchema(object.getClass());
+        Assert.isTrue(schema.getFields().equals(record.getSchema().getFields()), "Schema fields didn't match");
+        record.getSchema().getFields().forEach(d -> PropertyAccessorFactory.forDirectFieldAccess(object).setPropertyValue(d.name(), record.get(d.name()) == null ? record.get(d.name()) : record.get(d.name()).toString()));
+        return object;
+    }
+}
+
+class LocationGroup {
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCardnumber() {
+        return cardnumber;
+    }
+
+    public void setCardnumber(String cardnumber) {
+        this.cardnumber = cardnumber;
+    }
+
+    private String zipcode;
+    private String cardnumber;
+
 }
